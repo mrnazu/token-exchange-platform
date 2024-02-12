d0cc8940bea7cf4c8c88d041aace3255ca8d910d
mrnazu <samuelamsalu1998@gmail.com>
2024-02-12 13:56:35 -0800

    Add task to print accounts.. with dontenv config

--- frontend/hardhat.config.cjs
+++ frontend/hardhat.config.cjs
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts){
+    console.log(account.address);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
 };
+
