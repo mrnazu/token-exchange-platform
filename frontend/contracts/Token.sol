97f71770da6b1fa81f635f5a457ea0a381b26e34
mrnazu <samuelamsalu1998@gmail.com>
2024-03-04 08:07:42 -0800

    #ISSUE - A bit version correction, I removed some dev dependencies and Token.js refactored

--- frontend/contracts/Token.sol
+++ frontend/contracts/Token.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.19;
 
 contract Token {
     string public name = "My Token Here"; // public global var
+    string public symbol = "NAZU"
 }
--- frontend/scripts/Token.js
+++ frontend/scripts/Token.js
@@ -3,10 +3,13 @@ const ethers = require('hardhat');
 const expect = require('chai');
 
 describe("Token", function() {
-    it("Baisc Token with Name", async function() {
+    beforeEach( async function(){
         // fetch token from blockchain
         const Token = await ethers.getContractFactory("Token") // Import
         let token = await Token.deploy(); // deployed
+    })
+
+    it("Has Baisc Token with Name", async function() {
         // read
         const Name = await Token.name(); // get the token Name
         // check the name
@@ -14,4 +17,15 @@ describe("Token", function() {
         // or
         expect(Name).to.equal('My Token Here');
     });
+
+    it("Has Baisc Token with Symbol", async function() {
+        // read
+        const Symbol = await Token.symbol(); // get the token Symbol
+        // check the Symbol
+        console.log(Symbol); // log the Symbol
+        // or
+        expect(Symbol).to.equal('NAZU');
+    });
 });
+
+// 3. Testing Contract: 20:30
