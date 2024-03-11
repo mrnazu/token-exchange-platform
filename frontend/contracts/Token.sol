d3e3f3ce0343e17c3bb1f6fc7a598ddd8d835f3e
mrnazu <samuelamsalu1998@gmail.com>
2024-03-11 13:23:12 -0700

    feat: Resolve dependency conflicts and update packages
    - Updated package.json dependencies to resolve conflicts and ensure compatibility
    - Removed specific version constraints for @nomicfoundation/hardhat-toolbox to allow automatic resolution of dependencies
    - Fixed errors related to ethers.getContractFactory not being a function in test scripts
    - Cleaned up unnecessary files and directories causing errors

--- frontend/contracts/Token.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19; 
-
-contract Token {
-    string public name = "My Token Here"; // public global var
-    string public symbol = "NAZU"
-}
