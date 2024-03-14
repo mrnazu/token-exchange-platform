bd7795d23ea1e14f994b9b64f9997bc0963918b4
mrnazu <samuelamsalu1998@gmail.com>
2024-03-14 03:00:32 -0700

    fix: Update Solidity compiler version in Hardhat configuration
    Updated the Solidity compiler version in the Hardhat configuration file (hardhat.config.js) to match the current compiler version (0.6.12). This resolves the version incompatibility issue encountered during contract compilation. Now, the Solidity code can be compiled successfully using the correct compiler version specified in the configuration.

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
+// eslint-disable-next-line no-undef
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,6 +11,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
